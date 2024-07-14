2cbc525a3d770f9ce8d46c0f5fc795e44c50bebe
Aaron Dbritto <143990364+Adbritto@users.noreply.github.com>
2024-07-14 16:48:08 -0700

    add spritesheet support

--- src/main/java/emerald/LevelEditorScene.java
+++ src/main/java/emerald/LevelEditorScene.java
@@ -1,6 +1,8 @@
 package emerald;
 
+import components.Sprite;
 import components.SpriteRenderer;
+import components.Spritesheet;
 import emerald.util.AssetPool;
 import org.joml.Vector2f;
 
@@ -12,21 +14,27 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void init() {
+        loadResources();
+
         this.camera = new Camera(new Vector2f());
 
+        Spritesheet sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
+
         GameObject obj1 = new GameObject("Object 1", new Transform(new Vector2f(100,100), new Vector2f(256, 256)));
-        obj1.addComponent(new SpriteRenderer(AssetPool.getTexture("assets/images/bhargie.jpg")));
+        obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
         this.addGameObjectToScene(obj1);
 
         GameObject obj2 = new GameObject("Object 2", new Transform(new Vector2f(400,100), new Vector2f(256, 256)));
-        obj2.addComponent(new SpriteRenderer(AssetPool.getTexture("assets/images/shine.jpg")));
+        obj2.addComponent(new SpriteRenderer(sprites.getSprite(6)));
         this.addGameObjectToScene(obj2);
-
-        loadResources();
     }
 
     private void loadResources() {
         AssetPool.getShader("assets/shaders/default.glsl");
+
+        AssetPool.addSpritesheet("assets/images/spritesheet.png",
+                new Spritesheet(AssetPool.getTexture("assets/images/spritesheet.png"),
+                        16, 16, 26, 0));
     }
 
     @Override
