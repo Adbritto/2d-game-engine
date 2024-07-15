630ff43fa6ec8be0ba4165372fb57d9eeb1dcd67
Aaron Dbritto <143990364+Adbritto@users.noreply.github.com>
2024-07-14 21:18:40 -0700

    add ImGui and integrate into window

--- src/main/java/emerald/Window.java
+++ src/main/java/emerald/Window.java
@@ -1,6 +1,13 @@
 package emerald;
 
+import imgui.ImGui;
+import imgui.ImGuiIO;
+import imgui.flag.ImGuiConfigFlags;
+import imgui.gl3.ImGuiImplGl3;
+import imgui.glfw.ImGuiImplGlfw;
 import org.lwjgl.Version;
+import org.lwjgl.glfw.Callbacks;
+import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 
@@ -10,7 +17,8 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Window {
-    private final int width, height;
+    private int width;
+    private int height;
     private final String title;
     private long glfwWindow;
 
@@ -19,6 +27,12 @@ public class Window {
     private  static Window window = null;
 
     private static Scene currentScene;
+
+    private final ImGuiImplGlfw imGuiGlfw = new ImGuiImplGlfw();
+    private final ImGuiImplGl3 imGuiGl3 = new ImGuiImplGl3();
+
+    private String glslVersion = null;
+    private ImGuiLayer imGuiLayer;
 
     private  Window() {
         this.width = 1280;
@@ -64,56 +78,15 @@ public class Window {
 
         init();
         loop();
+        destroy();
 
-        // Free the memory
-        glfwFreeCallbacks(glfwWindow);
-        glfwDestroyWindow(glfwWindow);
-
-        // Terminate GLFW
-        glfwTerminate();
-        glfwSetErrorCallback(null).free();
     }
 
     public void init() {
-        // Setup error callback
-        GLFWErrorCallback.createPrint(System.err).set();
+        initWindow();
 
-        // Init GLFW
-        if (!glfwInit()) {
-            throw new IllegalStateException("Unable to Initialize GLFW.");
-        }
-
-        // Config GLFW
-        glfwDefaultWindowHints();
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
-
-        // Create Window
-        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
-        if (glfwWindow == NULL) {
-            throw new IllegalStateException("Failed to create window.");
-        }
-
-        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
-        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
-        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
-        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
-
-        // Make OpenGL the context current
-        glfwMakeContextCurrent(glfwWindow);
-        // Enable v-sync
-        glfwSwapInterval(1);
-
-        // Make window visible
-        glfwShowWindow(glfwWindow);
-
-        GL.createCapabilities();
-
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
-
-        Window.changeScene(0);
+        imGuiGlfw.init(glfwWindow, true);
+        imGuiGl3.init(glslVersion);
     }
 
     public  void loop() {
@@ -132,11 +105,96 @@ public class Window {
                 currentScene.update(dt);
             }
 
+            this.imGuiLayer.update(dt);
             glfwSwapBuffers(glfwWindow);
 
             endTime = (float)glfwGetTime();
             dt = endTime - beginTime;
             beginTime = endTime;
         }
+    }
+
+    public void destroy() {
+        imGuiGl3.dispose();
+        imGuiGlfw.dispose();
+        ImGui.destroyContext();
+
+        // Free Memory
+        Callbacks.glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        // Terminate GLFW
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    private void initWindow() {
+        // Setup error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Init GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to Initialize GLFW.");
+        }
+
+        // Config GLFW
+        glslVersion = "#version 330 core";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // Create Window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create window.");
+        }
+
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+        glfwSetWindowSizeCallback(glfwWindow, (w, newWidth, newHeight) -> {
+            Window.setWidth(newWidth);
+            Window.setHeight(newHeight);
+        } );
+
+        // Make OpenGL the context current
+        glfwMakeContextCurrent(glfwWindow);
+
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make window visible
+        glfwShowWindow(glfwWindow);
+
+        GL.createCapabilities();
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+        this.imGuiLayer = new ImGuiLayer(glfwWindow);
+        this.imGuiLayer.initImGui();
+
+        Window.changeScene(0);
+    }
+
+
+
+    public static int getWidth() {
+        return get().width;
+    }
+
+    public static int getHeight() {
+        return get().height;
+    }
+
+    public static void setWidth(int newWidth) {
+        get().width = newWidth;
+    }
+
+    public static void setHeight(int newHeight) {
+        get().height = newHeight;
     }
 }
