7e8dfe2f6ac8aab6a992a2e73cbf4b31bf6c1ecd
Aaron Dbritto <143990364+Adbritto@users.noreply.github.com>
2024-07-23 16:24:02 -0700

    Add link for next video
    TODO: reset this commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,3 +6,5 @@ public class Main {
         window.run();
     }
 }
+// Continue after adding physics
+// https://www.youtube.com/watch?v=lALvR4j6RCM&list=PLtrSb4XxIVbp8AKuEAlwNXDxr99e3woGE&index=28
