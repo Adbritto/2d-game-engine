01d5dd7f5f48e8b9e612acdec4c116666baf5231
Aaron Dbritto <143990364+Adbritto@users.noreply.github.com>
2024-07-23 16:19:46 -0700

    Add box and circle methods to draw class

--- src/main/java/emerald/scenes/LevelEditorScene.java
+++ src/main/java/emerald/scenes/LevelEditorScene.java
@@ -66,10 +66,16 @@ public class LevelEditorScene extends Scene {
         AssetPool.getTexture("assets/images/blendImage2.png");
     }
 
+    float x = 0.0f;
+    float y = 0.0f;
     @Override
     public void update(float dt) {
 //        System.out.println("FPS: " + (1.0f / dt));
         levelEditorUtils.update(dt);
+        DebugDraw.addCircle(new Vector2f(x, y), 64, new Vector3f(0,1,0), 1);
+
+        x += 50 * dt;
+        y += 30 * dt;
 
         for (GameObject go : this.gameObjects) {
             go.update(dt);
